8a46f318ba5fdf50af3345b152599ead9fb1935b
Kiara Eunice Abamonga <kiara.abamonga@gmail.com>
2025-06-10 13:17:15 -0500

    Initial Commit

---
+++ problemSolving/problemSolvingApp.swift
@@ -0,0 +1,17 @@
+//
+//  problemSolvingApp.swift
+//  problemSolving
+//
+//  Created by Kiara on 6/10/25.
+//
+
+import SwiftUI
+
+@main
+struct problemSolvingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
