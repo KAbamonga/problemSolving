c186d6098b980e358ffd140d8f3e0c940b62e2e5
Kiara Eunice Abamonga <kiara.abamonga@gmail.com>
2025-06-11 13:53:02 -0500

    june to july change

---
+++ problemSolving/textCutoff.swift
@@ -0,0 +1,41 @@
+
+// TextTopper.swift
+// 1.7 Problem Solving
+//
+// Created by Valerie Hammer on 6/10/25.
+//
+import SwiftUI
+struct textCutoff: View {
+  var body: some View {
+    VStack() {
+      Text("JULY")
+        .font(.largeTitle)
+        .foregroundColor(Color.white)
+        .padding()
+        .fontWeight(.bold)
+        .background(Color.blue)
+      Spacer()
+        .frame(height: 180)
+      Text("You have everything it takes inside you already—so start now, trust your path, and go after your dreams like they were made just for you. Because they were.")
+        .font(.title)
+        .padding()
+        .background(Color.blue)
+        .foregroundColor(.white)
+        .cornerRadius(10)
+      Spacer()
+        .frame(height:190)
+    }
+    //end VStack
+    .padding(.horizontal, 37.0)
+  }
+}
+#Preview {
+    textCutoff()
+}
+
+
+
+
+
+
+
